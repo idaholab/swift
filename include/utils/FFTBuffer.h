cd097ffda29579266a2428a5b8a4caf92f02716c
Daniel Schwen <daniel.schwen@inl.gov>
2024-05-24 16:47:54 -0600

    expand API

--- include/utils/FFTBuffer.h
+++ include/utils/FFTBuffer.h
@@ -16,32 +16,161 @@
 
 using neml2::TorchShapeRef;
 
-template <typename T>
+namespace MooseFFT
+{
+
+template <typename T, int D>
+class FFTBuffer;
+
+template <typename T = neml2::Scalar>
+FFTBuffer<T, 1>
+create1DBuffer(int nx, const torch::TensorOptions & options = neml2::default_tensor_options())
+{
+  return FFTBuffer<T, 1>(TorchShapeRef({nx}), options);
+}
+
+template <typename T = neml2::Scalar>
+FFTBuffer<T, 2>
+create2DBuffer(int nx,
+               int ny,
+               const torch::TensorOptions & options = neml2::default_tensor_options())
+{
+  return FFTBuffer<T, 2>(TorchShapeRef({nx, ny}), options);
+}
+
+template <typename T = neml2::Scalar>
+FFTBuffer<T, 3>
+create3DBuffer(int nx,
+               int ny,
+               int nz,
+               const torch::TensorOptions & options = neml2::default_tensor_options())
+{
+  return FFTBuffer<T, 3>(TorchShapeRef({nx, ny, nz}), options);
+}
+
+enum class Interval
+{
+  CLOSED,
+  OPEN,
+  LEFT_OPEN,
+  RIGHT_OPEN
+};
+
+template <typename T, int D>
 class FFTBuffer
 {
+  FFTBuffer(const TorchShapeRef & batch_shape, const torch::TensorOptions & options);
+
 public:
-  FFTBuffer(const TorchShapeRef & batch_shape,
-            const torch::TensorOptions & options = default_tensor_options());
-
-  FFTBuffer(int... size, const torch::TensorOptions & options = default_tensor_options())
-    : FFTBuffer(TorchShapeRef{size...}, options)
-  {
-  }
-
   T & data() { return _data; }
   const T & data() const { return _data; }
 
+  std::array<neml2::Real, D> & min() { return _min; }
+  std::array<neml2::Real, D> & max() { return _max; }
+
+  neml2::Scalar getAxis(int dim, Interval interval = Interval::LEFT_OPEN) const;
+  neml2::Scalar getFrequency(int dim) const;
+
+  auto getFrequencies() const { return getFrequenciesHelper(std::make_integer_sequence<int, D>{}); }
+
+  // stream in gnuplot readable format
+
+  friend FFTBuffer<T, 1> create1DBuffer<T>(int nx, const torch::TensorOptions & options);
+  friend FFTBuffer<T, 2> create2DBuffer<T>(int nx, int ny, const torch::TensorOptions & options);
+  friend FFTBuffer<T, 3>
+  create3DBuffer<T>(int nx, int ny, int nz, const torch::TensorOptions & options);
+
+protected:
+  template <int... dims>
+  auto getFrequenciesHelper(std::integer_sequence<int, dims...>) const
+  {
+    return std::make_tuple(getFrequency(dims)...);
+  }
+
 private:
   T _data;
+  const bool _rfft;
+  std::array<neml2::Real, D> _min;
+  std::array<neml2::Real, D> _max;
 };
 
-template <typename T>
-FFTBuffer::FFTBuffer(const TorchShapeRef & batch_shape,
-                     const torch::TensorOptions & options = default_tensor_options())
-  : _data(T::zeroes(batch_shape, options))
+template <typename T, int D>
+FFTBuffer<T, D>::FFTBuffer(const TorchShapeRef & batch_shape, const torch::TensorOptions & options)
+  : _data(T::zeros(batch_shape, options)), _rfft(true)
 {
+  if (batch_shape.size() != D)
+    throw std::domain_error("Invalid dimension");
 }
 
-typedef FFTBuffer<Real> FFTRealBuffer;
+template <typename T, int D>
+neml2::Scalar
+FFTBuffer<T, D>::getAxis(int dim, Interval interval) const
+{
+  if (dim < 0 || dim >= D)
+    throw std::domain_error("Invalid dimension");
+
+  const auto n = _data.batch_sizes()[dim];
+
+  switch (interval)
+  {
+    case Interval::OPEN:
+      return torch::unsqueeze(
+          torch::narrow(
+              neml2::Scalar::linspace(neml2::Scalar(_min[dim], neml2::default_tensor_options()),
+                                      neml2::Scalar(_max[dim], neml2::default_tensor_options()),
+                                      n + 2),
+              0,
+              1,
+              n),
+          D - dim - 1);
+
+    case Interval::LEFT_OPEN:
+      return torch::unsqueeze(
+          torch::narrow(
+              neml2::Scalar::linspace(neml2::Scalar(_min[dim], neml2::default_tensor_options()),
+                                      neml2::Scalar(_max[dim], neml2::default_tensor_options()),
+                                      n + 1),
+              0,
+              1,
+              n),
+          D - dim - 1);
+
+    case Interval::RIGHT_OPEN:
+      return torch::unsqueeze(
+          torch::narrow(
+              neml2::Scalar::linspace(neml2::Scalar(_min[dim], neml2::default_tensor_options()),
+                                      neml2::Scalar(_max[dim], neml2::default_tensor_options()),
+                                      n + 1),
+              0,
+              0,
+              n),
+          D - dim - 1);
+
+    default: // case CLOSED:
+      return torch::unsqueeze(
+          neml2::Scalar::linspace(neml2::Scalar(_min[dim], neml2::default_tensor_options()),
+                                  neml2::Scalar(_max[dim], neml2::default_tensor_options()),
+                                  n),
+          D - dim - 1);
+  }
+}
+
+template <typename T, int D>
+neml2::Scalar
+FFTBuffer<T, D>::getFrequency(int dim) const
+{
+  if (dim < 0 || dim >= D)
+    throw std::domain_error("Invalid dimension");
+
+  const auto n = _data.batch_sizes()[dim];
+  const auto a = (_max[dim] - _min[dim]) / Real(n);
+  const auto freq = (dim == 0 && _rfft)
+                        ? torch::fft::rfftfreq(n, a, neml2::default_tensor_options())
+                        : torch::fft::fftfreq(n, a, neml2::default_tensor_options());
+
+  return torch::unsqueeze(freq, dim);
+}
+
+}
 
 #endif
--- unit/src/FFTBufferTest.C
+++ unit/src/FFTBufferTest.C
@@ -14,10 +14,82 @@
 
 TEST(FFTBuffer, Scalar)
 {
-  FFTBuffer<Scalar> a({16, 6});
-  FFTBuffer<Scalar> b(16, 6);
+  auto a = MooseFFT::create2DBuffer(10, 10);
+  const auto pi = libMesh::pi;
+  a.min() = {-pi, -pi};
+  a.max() = {pi, pi};
+
+  auto x = a.getAxis(0);
+  auto y = a.getAxis(1);
+  auto A = a.data();
+
+  A = sin(2.0 * x) + 0.1 * sin(3.0 * y);
+
+  auto Abar = torch::fft::rfft2(A);
+
+  std::cout << Abar << '\n';
+
+  auto [i, j] = a.getFrequencies();
+
+  std::cout << i << '\n';
+  std::cout << j << '\n';
+
+  // std::cout << A << '\n';
+}
+
+TEST(FFTBuffer, 2DAxis)
+{
+  auto a = MooseFFT::create2DBuffer(3, 4);
+  a.min() = {0.0, 0.0};
+  a.max() = {3.0 * 4.0 * 5.0, 4.0 * 5.0 * 6.0};
+
+  const auto xCompare = [&a](auto interval, torch::detail::TensorDataContainer gold)
+  {
+    auto xt = a.getAxis(0, interval);
+    auto xg = torch::unsqueeze(torch::tensor(gold), 1);
+    EXPECT_TRUE(torch::equal(xt, xg));
+  };
+
+  const auto yCompare = [&a](auto interval, torch::detail::TensorDataContainer gold)
+  {
+    auto yt = a.getAxis(1, interval);
+    auto yg = torch::unsqueeze(torch::tensor(gold), 0);
+    EXPECT_TRUE(torch::equal(yt, yg));
+  };
+
+  xCompare(MooseFFT::Interval::CLOSED, {0.0, 30.0, 60.0});
+  xCompare(MooseFFT::Interval::OPEN, {15.0, 30.0, 45.0});
+  xCompare(MooseFFT::Interval::LEFT_OPEN, {20.0, 40.0, 60.0});
+  xCompare(MooseFFT::Interval::RIGHT_OPEN, {0.0, 20.0, 40.0});
+
+  yCompare(MooseFFT::Interval::CLOSED, {0.0, 40.0, 80.0, 120.0});
+  yCompare(MooseFFT::Interval::OPEN, {24.0, 48.0, 72.0, 96.0});
+  yCompare(MooseFFT::Interval::LEFT_OPEN, {30.0, 60.0, 90.0, 120.0});
+  yCompare(MooseFFT::Interval::RIGHT_OPEN, {0.0, 30.0, 60.0, 90.0});
+}
+
+TEST(FFTBuffer, 1DAxis)
+{
+  auto a = MooseFFT::create1DBuffer(4);
+  a.min() = {0.0};
+  a.max() = {4.0 * 5.0 * 6.0};
+
+  const auto xCompare = [&a](auto interval, torch::detail::TensorDataContainer gold)
+  {
+    auto xt = a.getAxis(0, interval);
+    auto xg = torch::unsqueeze(torch::tensor(gold), 0);
+    EXPECT_TRUE(torch::equal(xt, xg));
+  };
+
+  xCompare(MooseFFT::Interval::CLOSED, {0.0, 40.0, 80.0, 120.0});
+  xCompare(MooseFFT::Interval::OPEN, {24.0, 48.0, 72.0, 96.0});
+  xCompare(MooseFFT::Interval::LEFT_OPEN, {30.0, 60.0, 90.0, 120.0});
+  xCompare(MooseFFT::Interval::RIGHT_OPEN, {0.0, 30.0, 60.0, 90.0});
 }
 
 #else
-#warning "SHIT, no NEML2!"
+
+#warning "NEML2 not found, skipping FFTBuffer unit test."
+
 #endif
+
