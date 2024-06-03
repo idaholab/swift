471bd2f93d5f433386b0ea89628938ae60e5f395
Daniel Schwen <daniel.schwen@inl.gov>
2024-06-03 09:07:44 -0600

    fix env

--- env.sh
+++ env.sh
@@ -6,5 +6,5 @@ conda deactivate
 export HDF5_STR='--download-hdf5=1 --with-hdf5-fortran-bindings=0 --download-hdf5-configure-arguments="--with-zlib"'
 export LIBMESH_DIR="$HOME/Programs/libmesh/installed"
 export LIBMESH_SRC_DIR="$HOME/Programs/libmesh"
-export MOOSE_DIR="$HOME/Programs/moose-GPU"
+export MOOSE_DIR=$(realpath $HOME/Programs/moose-GPU* | tail -n 1)
 
