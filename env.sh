2ebcd0b2cc83f688330e89bb470c952275d01da1
Daniel Schwen <daniel@schwen.de>
2024-06-02 20:49:57 -0600

    add env script

---
+++ env.sh
@@ -0,0 +1,10 @@
+conda deactivate
+conda deactivate
+conda deactivate
+conda deactivate
+
+export HDF5_STR='--download-hdf5=1 --with-hdf5-fortran-bindings=0 --download-hdf5-configure-arguments="--with-zlib"'
+export LIBMESH_DIR="$HOME/Programs/libmesh/installed"
+export LIBMESH_SRC_DIR="$HOME/Programs/libmesh"
+export MOOSE_DIR="$HOME/Programs/moose-GPU"
+
