d0db822f279ce4a9e35e5bf3e542ebd2edfa0a5a
Nijat Nazim Rustamov <nrustamo@dhcp-10-2-224-229.uwyo.edu>
2024-12-23 21:16:05 -0700

    add lbm stencils

---
+++ include/stencils/LBMStencilBase.h
@@ -0,0 +1,48 @@
+/**********************************************************************/
+/*                    DO NOT MODIFY THIS HEADER                       */
+/*             Swift, a Fourier spectral solver for MOOSE             */
+/*                                                                    */
+/*            Copyright 2024 Battelle Energy Alliance, LLC            */
+/*                        ALL RIGHTS RESERVED                         */
+/**********************************************************************/
+
+#pragma once
+
+#include "torch/torch.h"
+
+#include "MooseObject.h"
+#include "SwiftTypes.h"
+#include "SwiftUtils.h"
+
+/**
+ * Base class for lattice stencils
+ */
+
+class LBMStencilBase : public MooseObject
+{
+public:
+  static InputParameters validParams();
+
+  LBMStencilBase(const InputParameters & parameters);
+
+public:
+  int64_t _q;
+  torch::Tensor _ex;
+  torch::Tensor _ey;
+  torch::Tensor _ez;
+  torch::Tensor _weights;
+  torch::Tensor _M;
+  torch::Tensor _M_inv;
+  torch::Tensor _S;
+  
+  // incoming unknown distribution functions at every face
+  torch::Tensor _top;
+  torch::Tensor _bottom;
+  torch::Tensor _left;
+  torch::Tensor _right;
+  torch::Tensor _front;
+  torch::Tensor _back;
+
+  // indices of opposite directions
+  torch::Tensor _op;
+};
---
+++ src/stencils/LBMStencilBase.C
@@ -0,0 +1,22 @@
+/**********************************************************************/
+/*                    DO NOT MODIFY THIS HEADER                       */
+/*             Swift, a Fourier spectral solver for MOOSE             */
+/*                                                                    */
+/*            Copyright 2024 Battelle Energy Alliance, LLC            */
+/*                        ALL RIGHTS RESERVED                         */
+/**********************************************************************/
+
+#include "LBMStencilBase.h"
+
+InputParameters
+LBMStencilBase::validParams()
+{
+  InputParameters params = MooseObject::validParams();
+  params.addClassDescription("LB Stencil object.");
+  params.registerBase("LBStencil");
+  return params;
+}
+
+LBMStencilBase::LBMStencilBase(const InputParameters & parameters) : MooseObject(parameters) 
+{
+}
