82eb4c2c47c07215411977b2155d67bf17ff5595
Nijat Nazim Rustamov <nrustamo@dhcp-10-2-224-229.uwyo.edu>
2025-01-03 13:55:29 -0700

    add lbm problem methods

--- include/problems/LatticeBoltzmannProblem.h
+++ include/problems/LatticeBoltzmannProblem.h
@@ -12,6 +12,10 @@
 
 #include "TensorProblem.h"
 
+
+class LBMStencilBase;
+class LatticeBoltzmannMesh;
+
 /**
  * Problem object for solving lattice Boltzmann problems
  */
@@ -22,9 +26,73 @@ public:
 
   LatticeBoltzmannProblem(const InputParameters & parameters);
 
+  void init() override;
+
+  void execute(const ExecFlagType & exec_type) override;
+  
+  // void advanceState() override;
+
   void addStencil(const std::string & stencil_name,
                               const std::string & name,
                               InputParameters & parameters);
+  
+  const LBMStencilBase & getStencil() const {return *_stencil; }
+
+  const bool & isSlipEnabled() const {return _enable_slip; }
+
+  const torch::Tensor & getSlipRelaxationMatrix() const {return _slip_relaxation_matrix;}
+
+  const int & getTotalSteps() const {return _t_total;}
+
+  /// sets up slip model
+  void enableSlipModel();
+
+  /// sets convergence residual
+  void setSolverResidual(const Real & residual);
+
+  /// sets tensor to a value at wall nodes
+  void setTensorToValue(torch::Tensor & t, const Real & value);
+
+protected:
+  /// LBM Mesh object
+  LatticeBoltzmannMesh * _lbm_mesh;
+
+  /// LBM stencils
+  std::shared_ptr<LBMStencilBase> _stencil;
+
+  /// buffers with extra dimension
+  std::map<std::string, unsigned int> _buffer_extra_dimension;
+
+  /// enables slip models
+  bool _enable_slip;
+
+  /// mean free path
+  Real _mfp;
+
+  /// resolution
+  Real _dx;
+
+  // slip coefficient
+  const Real _A = 0.6;
+
+  /// relaxation matrix as a funcion of Kn and local pore size in slip model
+  torch::Tensor _slip_relaxation_matrix;
+
+  /// used to restricts construction of more than one stencil object
+  unsigned int _stencil_counter = 0;
+
+  /// convergence residual
+  Real _convergence_residual;
+
+  /// total number of time steps taken
+  int _t_total = 0;
+
+public:
+  /// LBM constants
+  const Real _cs = 1.0 / sqrt(3.0);
+  const Real _cs2 = _cs * _cs;
+  const Real _cs4 = _cs2 * _cs2;
+
 };
 
 #endif
--- src/problems/LatticeBoltzmannProblem.C
+++ src/problems/LatticeBoltzmannProblem.C
@@ -7,6 +7,8 @@
 /**********************************************************************/
 
 #include "LatticeBoltzmannProblem.h"
+#include "LatticeBoltzmannMesh.h"
+#include "LBMStencilBase.h"
 
 registerMooseObject("SwiftApp", LatticeBoltzmannProblem);
 
@@ -14,6 +16,9 @@ InputParameters
 LatticeBoltzmannProblem::validParams()
 {
   InputParameters params = TensorProblem::validParams();
+  params.addParam<bool>("enable_slip", false, "Enable slip model");
+  params.addParam<Real>("mfp", 0.0, "Mean free path of the system, (meters)");
+  params.addParam<Real>("dx", 0.0, "Domain resolution, (meters)");
   params.addClassDescription(
       "Problem object to enable solving lattice Boltzmann problems");
 
@@ -21,8 +26,47 @@ LatticeBoltzmannProblem::validParams()
 }
 
 LatticeBoltzmannProblem::LatticeBoltzmannProblem(const InputParameters & parameters)
-    : TensorProblem(parameters)
+    : TensorProblem(parameters),
+    _lbm_mesh(dynamic_cast<LatticeBoltzmannMesh *>(&_mesh)),
+    _enable_slip(getParam<bool>("enable_slip")),
+    _mfp(getParam<Real>("mfp")),
+    _dx(getParam<Real>("dx"))
 {
+}
+
+void
+LatticeBoltzmannProblem::init()
+{
+  // initialize base class
+  TensorProblem::init();
+
+  // initialize buffers with extra dimensions
+  for (auto & pair : _tensor_buffer)
+  {
+    auto extra_dim = _buffer_extra_dimension.find(pair.first);
+
+    if (extra_dim->second >= 1)
+    {
+      // buffers with extra dimension
+      unsigned int dim = 4;
+      std::array<int64_t, 4> n;
+      std::fill(n.begin(), n.end(), 1);
+      std::copy(_n.begin(), _n.end(), n.begin());
+      n[dim - 1] = static_cast<int64_t>(extra_dim->second);
+      torch::IntArrayRef shape = torch::IntArrayRef(n.data(), dim);
+      pair.second = torch::zeros(shape, _options);
+    }
+  }
+
+  // set up parameters for slip flow
+  if (_enable_slip)
+    enableSlipModel();
+}
+
+void
+LatticeBoltzmannProblem::execute(const ExecFlagType & exec_type)
+{
+  
 }
 
 void
@@ -31,4 +75,82 @@ LatticeBoltzmannProblem::addStencil(
                              const std::string & name,
                              InputParameters & parameters)
 {
+  if (_stencil_counter > 0)
+    mooseError("Problem object LatticeBoltzmannProblem can only have one stencil");
+  // Create the object
+  _stencil = _factory.create<LBMStencilBase>(stencil_name, name, parameters, 0);
+  _stencil_counter ++;
+  logAdd("LBMStencilBase", name, stencil_name, parameters);
+}
+
+
+void
+LatticeBoltzmannProblem::enableSlipModel()
+{
+
+  const bool & is_mesh_vtk = _lbm_mesh->isMeshVTKFile();
+
+  if (!is_mesh_vtk)
+    mooseError("Knudsen and local pore size distributions must be provided with vtk file");
+
+  else
+  {
+    // shape of relaxation matrix
+    std::array<int64_t, 5> extended_shape = {_n[0], _n[1], _n[2], _stencil->_q, _stencil->_q};
+
+    // saves relaxation matrix for every mesh element in the domain
+    _slip_relaxation_matrix = torch::zeros(extended_shape, _options);
+
+    // retrieve Knudsen and Local pore sizes
+    const auto & Kn = _lbm_mesh->getKn();
+    const auto & pore_size = _lbm_mesh->getPoreSize();
+
+    // compute relaxation matrix
+    for (int i = 0; i < _shape[0]; i++)
+      for (int j = 0; j < _shape[1]; j++)
+        for (int k = 0; k < _shape[2]; k++)
+        {
+          Real pore_size_scalar = pore_size[i][j][k].item<Real>();
+          Real kn_scalar = Kn[i][j][k].item<Real>();
+
+          Real tau_s = 0.5 + sqrt(6.0 / M_PI) * pore_size_scalar * kn_scalar / (1 + 2 * kn_scalar);
+          Real tau_d = 0.5 + (3.0 / 2.0) * sqrt(3.0) * 1.0 / pow((1.0 /
+                      sqrt((_mfp / _dx * 1.0 / (1.0 + 2.0 * kn_scalar))) * 2.0), 2.0);
+          Real tau_q = 0.5 + (3.0 + M_PI * (2.0 * tau_s - 1.0) * (2.0 * tau_s - 1.0) * _A) / (8.0 * (2.0 * tau_s - 1.0));
+          torch::Tensor  relaxation_matrix = torch::diag(torch::tensor({1.0 / 1.0, 1.0 / 1.1, 1.0 / 1.2, 1.0 / tau_d, 1.0 / tau_q,
+                          1.0 / tau_d, 1.0 / tau_q,  1.0 / tau_s,  1.0 / tau_s}, _options));
+
+          _slip_relaxation_matrix.index_put_({i, j, k}, relaxation_matrix);
+        }
+  }
+}
+
+void
+LatticeBoltzmannProblem::setSolverResidual(const Real & residual)
+{
+  _convergence_residual = residual;
+}
+
+void
+LatticeBoltzmannProblem::setTensorToValue(torch::Tensor & t, const Real & value)
+{
+  auto tensor_shape = t.sizes();
+  const bool & is_mesh_file = _lbm_mesh->isMeshDatFile();
+  const bool & is_mesh_vtk = _lbm_mesh->isMeshVTKFile();
+  const auto & binary_mesh = _lbm_mesh->getBinaryMesh();
+  if (is_mesh_file || is_mesh_vtk)
+  {
+    if (t.dim() == binary_mesh.dim())
+    {
+      // 3D
+      const auto solid_mask = (binary_mesh == value);
+      t.masked_fill_(solid_mask, value);
+    }
+    else
+    {
+      // 2D and 1D
+      const auto solid_mask = (binary_mesh == value).unsqueeze(-1).expand(tensor_shape);
+      t.masked_fill_(solid_mask, value);
+    }
+  }
 }
