1325e47795e3d59738681a9ee51fbe2d294989ed
Nijat Nazim Rustamov <nrustamo@dhcp-10-2-224-229.uwyo.edu>
2025-01-08 19:48:37 -0700

    add bgk collision

---
+++ include/tensor_computes/LBMBGKCollision.h
@@ -0,0 +1,29 @@
+/**********************************************************************/
+/*                    DO NOT MODIFY THIS HEADER                       */
+/*             Swift, a Fourier spectral solver for MOOSE             */
+/*                                                                    */
+/*            Copyright 2024 Battelle Energy Alliance, LLC            */
+/*                        ALL RIGHTS RESERVED                         */
+/**********************************************************************/
+
+#pragma once
+
+#include "LatticeBoltzmannOperator.h"
+
+/**
+ * Single relaxation time BGK collision for Lattice Boltzmann Method
+ */
+class LBMBGKCollision : public LatticeBoltzmannOperator
+{
+public:
+  static InputParameters validParams();
+
+  LBMBGKCollision(const InputParameters & parameters);
+
+  void computeBuffer() override;
+
+protected:
+  Real  _tau_bgk;
+  const torch::Tensor _feq;
+  const torch::Tensor _f;
+};
---
+++ src/tensor_computes/LBMBGKCollision.C
@@ -0,0 +1,38 @@
+/**********************************************************************/
+/*                    DO NOT MODIFY THIS HEADER                       */
+/*             Swift, a Fourier spectral solver for MOOSE             */
+/*                                                                    */
+/*            Copyright 2024 Battelle Energy Alliance, LLC            */
+/*                        ALL RIGHTS RESERVED                         */
+/**********************************************************************/
+
+#include "LBMBGKCollision.h"
+#include "LatticeBoltzmannProblem.h"
+
+registerMooseObject("SwiftApp", LBMBGKCollision);
+
+InputParameters
+LBMBGKCollision::validParams()
+{
+  InputParameters params = LatticeBoltzmannOperator::validParams();
+  params.addClassDescription("Compute Object for single relaxation BGK collision for Lattice Boltzmann Method.");
+  params.addParam<Real>("tau", 1.0, "Relaxation parameter");
+  params.addRequiredParam<TensorInputBufferName>("feq", "Equilibrium distribution");
+  params.addRequiredParam<TensorInputBufferName>("f", "Pre-collision distribution");
+  return params;
+}
+
+LBMBGKCollision::LBMBGKCollision(const InputParameters & parameters)
+  : LatticeBoltzmannOperator(parameters),
+  _tau_bgk(getParam<Real>("tau")),
+  _feq(getInputBuffer("feq")),
+  _f(getInputBuffer("f"))
+{
+}
+
+void
+LBMBGKCollision::computeBuffer()
+{
+  _u = _f - 1.0 / _tau_bgk * (_f - _feq);
+  _lb_problem.setTensorToValue(_u, 0);
+}
