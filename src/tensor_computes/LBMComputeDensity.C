7baad0e87cfa92aecad145fa8fb7d710ba4f2c69
Nijat Nazim Rustamov <nrustamo@dhcp-10-2-224-229.uwyo.edu>
2025-01-10 16:14:22 -0700

    add compute density

---
+++ src/tensor_computes/LBMComputeDensity.C
@@ -0,0 +1,34 @@
+/**********************************************************************/
+/*                    DO NOT MODIFY THIS HEADER                       */
+/*             Swift, a Fourier spectral solver for MOOSE             */
+/*                                                                    */
+/*            Copyright 2024 Battelle Energy Alliance, LLC            */
+/*                        ALL RIGHTS RESERVED                         */
+/**********************************************************************/
+
+#include "LBMComputeDensity.h"
+#include "LatticeBoltzmannProblem.h"
+
+registerMooseObject("SwiftApp", LBMComputeDensity);
+
+InputParameters
+LBMComputeDensity::validParams()
+{
+  InputParameters params = LatticeBoltzmannOperator::validParams();
+  params.addRequiredParam<TensorInputBufferName>("f", "Distribution function");
+  params.addClassDescription("Compute object for macroscopic density reconstruction.");
+  return params;
+}
+
+LBMComputeDensity::LBMComputeDensity(const InputParameters & parameters)
+  : LatticeBoltzmannOperator(parameters),
+  _f(getInputBuffer("f"))
+{
+}
+
+void
+LBMComputeDensity::computeBuffer()
+{   
+  _u = torch::sum(_f, 3);
+  _lb_problem.setTensorToValue(_u, 0);
+}
