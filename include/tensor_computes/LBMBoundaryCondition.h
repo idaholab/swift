3dd4495157472b73d926fa51ae5b6106b8c8861e
Nijat Nazim Rustamov <nrustamo@dhcp-10-2-224-229.uwyo.edu>
2025-01-22 12:34:07 -0700

    add comments to explain boundary conventions

--- include/tensor_computes/LBMBoundaryCondition.h
+++ include/tensor_computes/LBMBoundaryCondition.h
@@ -21,6 +21,20 @@ public:
 
   LBMBoundaryCondition(const InputParameters & parameters);
 
+    /**
+   * Conventions:
+   * Left boundary is at x = 0
+   * Right is at x = Nx
+   * Front is at y = 0
+   * Back is at y = Ny
+   * Bottom is at z = 0
+   * Top is at z = Nz
+   * 
+   * Wall boundary refers to any obstacles in the domain
+   * It is entirely possible to put an obstacle at left, right, front, back, top or 
+   * bottom boundary and use wall boundary to replace any of them.
+   */
+
   virtual void topBoundary() = 0;
   virtual void bottomBoundary() = 0;
   virtual void leftBoundary() = 0;
