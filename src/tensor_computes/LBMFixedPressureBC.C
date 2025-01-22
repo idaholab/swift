cc43af480d72ed1e5954a14ca2dbc44bd038b3f9
Nijat Nazim Rustamov <nrustamo@dhcp-10-2-224-229.uwyo.edu>
2025-01-22 11:51:11 -0700

    add front and back pressure BC and fix left and right

--- src/tensor_computes/LBMFixedPressureBC.C
+++ src/tensor_computes/LBMFixedPressureBC.C
@@ -70,7 +70,7 @@ LBMFixedPressureBCTempl<2>::leftBoundary()
   for (unsigned int i = 1; i < _stencil._left.size(0); i++)
   {
     const auto & opposite_dir = _stencil._op[_stencil._left[i]];
-    _u.index_put_({0, Slice(), Slice(), _stencil._left[i]}, _f_old[0].index({0, Slice(), Slice(), opposite_dir}) + 0.5 * _stencil._ex[_stencil._left[i]] * 
+    _u.index_put_({0, Slice(), Slice(), _stencil._left[i]}, _f_old[0].index({0, Slice(), Slice(), opposite_dir}) - 0.5 * _stencil._ey[_stencil._left[i]] * 
                                                           (_f_old[0].index({0, Slice(), Slice(), 2}) - _f_old[0].index({0, Slice(), Slice(), 4})) + 
                                                           1.0/6.0 * _density * velocity );
   }
@@ -83,28 +83,27 @@ LBMFixedPressureBCTempl<3>::leftBoundary()
   // TBD
 }
 
-
 template <>
 void
 LBMFixedPressureBCTempl<2>::rightBoundary()
 {
-  torch::Tensor velocity = (_f_old[0].index({0, Slice(), Slice(), 0}) + 
-                        _f_old[0].index({0, Slice(), Slice(), 2}) + 
-                        _f_old[0].index({0, Slice(), Slice(), 4}) + 
-                        2 * (_f_old[0].index({0, Slice(), Slice(), 1}) + 
-                        _f_old[0].index({0, Slice(), Slice(), 5}) + 
-                        _f_old[0].index({0, Slice(), Slice(), 8}))) / _density - 1.0;
+  torch::Tensor velocity = (_f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), 0}) + 
+                        _f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), 2}) + 
+                        _f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), 4}) + 
+                        2 * (_f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), 1}) + 
+                        _f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), 5}) + 
+                        _f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), 8}))) / _density - 1.0;
   
   // axis aligned direction
   const auto & opposite_dir = _stencil._op[_stencil._left[0]];
-  _u.index_put_({0, Slice(), Slice(), opposite_dir}, _f_old[0].index({0, Slice(), Slice(), _stencil._left[0]}) - 2.0/3.0 * _density * velocity);
+  _u.index_put_({_grid_size[0] - 1, Slice(), Slice(), opposite_dir}, _f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), _stencil._left[0]}) - 2.0/3.0 * _density * velocity);
 
   // other directions
   for (unsigned int i = 1; i < _stencil._left.size(0); i++)
   {
     const auto & opposite_dir = _stencil._op[_stencil._left[i]];
-    _u.index_put_({0, Slice(), Slice(), opposite_dir}, _f_old[0].index({0, Slice(), Slice(), _stencil._left[i]}) + 0.5 * _stencil._ex[opposite_dir] * 
-                                                          (_f_old[0].index({0, Slice(), Slice(), 4}) - _f_old[0].index({0, Slice(), Slice(), 2})) - 
+    _u.index_put_({_grid_size[0] - 1, Slice(), Slice(), opposite_dir}, _f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), _stencil._left[i]}) + 0.5 * _stencil._ey[opposite_dir] * 
+                                                          (_f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), 4}) - _f_old[0].index({_grid_size[0] - 1, Slice(), Slice(), 2})) - 
                                                           1.0/6.0 * _density * velocity );
   }
 }
@@ -116,16 +115,68 @@ LBMFixedPressureBCTempl<3>::rightBoundary()
   // TBD
 }
 
-template <int dimension>
+template <>
 void
-LBMFixedPressureBCTempl<dimension>::frontBoundary()
+LBMFixedPressureBCTempl<2>::frontBoundary()
 {
+  torch::Tensor velocity = 1.0 - (_f_old[0].index({Slice(), 0, Slice(), 0}) + 
+                        _f_old[0].index({Slice(), 0, Slice(), 1}) + 
+                        _f_old[0].index({Slice(), 0, Slice(), 3}) + 
+                        2 * (_f_old[0].index({Slice(), 0, Slice(), 4}) + 
+                        _f_old[0].index({Slice(), 0, Slice(), 7}) + 
+                        _f_old[0].index({Slice(), 0, Slice(), 8}))) / _density;
+  
+  // axis aligned direction
+  const auto & opposite_dir = _stencil._op[_stencil._front[0]];
+  _u.index_put_({Slice(), 0, Slice(), _stencil._front[0]}, _f_old[0].index({Slice(), 0, Slice(), opposite_dir}) + 2.0/3.0 * _density * velocity);
+
+  // other directions
+  for (unsigned int i = 1; i < _stencil._front.size(0); i++)
+  {
+    const auto & opposite_dir = _stencil._op[_stencil._front[i]];
+    _u.index_put_({Slice(), 0, Slice(), _stencil._front[i]}, _f_old[0].index({Slice(), 0, Slice(), opposite_dir}) - 0.5 * _stencil._ex[_stencil._front[i]] * 
+                                                          (_f_old[0].index({Slice(), 0, Slice(), 1}) - _f_old[0].index({Slice(), 0, Slice(), 3})) + 
+                                                          1.0/6.0 * _density * velocity );
+  }
 }
 
-template <int dimension>
+template <>
 void
-LBMFixedPressureBCTempl<dimension>::backBoundary()
+LBMFixedPressureBCTempl<3>::frontBoundary()
 {
+  // TBD
+}
+
+template <>
+void
+LBMFixedPressureBCTempl<2>::backBoundary()
+{
+  torch::Tensor velocity = (_f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), 0}) + 
+                        _f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), 1}) + 
+                        _f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), 3}) + 
+                        2 * (_f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), 2}) + 
+                        _f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), 5}) + 
+                        _f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), 6}))) / _density - 1.0;
+  
+  // axis aligned direction
+  const auto & opposite_dir = _stencil._op[_stencil._front[0]];
+  _u.index_put_({Slice(), _grid_size[1] - 1, Slice(), opposite_dir}, _f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), _stencil._front[0]}) - 2.0/3.0 * _density * velocity);
+
+  // other directions
+  for (unsigned int i = 1; i < _stencil._front.size(0); i++)
+  {
+    const auto & opposite_dir = _stencil._op[_stencil._front[i]];
+    _u.index_put_({Slice(), _grid_size[1] - 1, Slice(), opposite_dir}, _f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), _stencil._front[i]}) + 0.5 * _stencil._ey[opposite_dir] * 
+                                                          (_f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), 4}) - _f_old[0].index({Slice(), _grid_size[1] - 1, Slice(), 2})) - 
+                                                          1.0/6.0 * _density * velocity );
+  }
+}
+
+template <>
+void
+LBMFixedPressureBCTempl<3>::backBoundary()
+{
+  // TBD
 }
 
 template <int dimension>
