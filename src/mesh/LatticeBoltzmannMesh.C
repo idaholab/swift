0a72f1de7b5b6330a3406b3af0d34c47807fc622
Nijat Nazim Rustamov <nrustamo@uwy.edu>
2025-03-24 14:01:40 -0600

    add mesh readers

--- src/mesh/LatticeBoltzmannMesh.C
+++ src/mesh/LatticeBoltzmannMesh.C
@@ -8,6 +8,17 @@
 
 #include "LatticeBoltzmannMesh.h"
 
+// VTK includes
+#ifdef LIBMESH_HAVE_VTK
+
+#include "vtkSmartPointer.h"
+#include "vtkXMLStructuredGridReader.h"
+#include "vtkStructuredGrid.h"
+#include "vtkPointData.h"
+#include "vtkDataArray.h"
+
+#endif
+
 registerMooseObject("SwiftApp", LatticeBoltzmannMesh);
 
 InputParameters
@@ -48,9 +59,8 @@ LatticeBoltzmannMesh::buildMesh()
     _binary_mesh = torch::ones({_nx, _ny, _nz}, MooseTensor::intTensorOptions());
     _local_pore_size = torch::ones({_nx, _ny, _nz}, MooseTensor::floatTensorOptions());
     _Knudsen_number = torch::ones({_nx, _ny, _nz}, MooseTensor::floatTensorOptions());
-    loadMeshFromVTKFile();
+    loadMeshFromVTKFile(_mesh_file, _binary_mesh, _local_pore_size, _Knudsen_number);
   }
-    
 }
 
 void
@@ -67,39 +77,61 @@ LatticeBoltzmannMesh::loadMeshFromDatFile()
     mooseError("Cannot open file " + _mesh_file);
 
   // read mesh into standart vector
-  std::vector<std::vector<int>> matrixData;
-  std::string line;
-  while (std::getline(file, line))
+  std::vector<int> fileData(_nx * _ny * _nz);
+  for (int i = 0; i < fileData.size(); i++)
   {
-    std::istringstream iss(line);
-    int num;
-    std::vector<int> row;
-    while (iss >> num)
-        row.push_back(num);
-    matrixData.push_back(row);
+    if (!(file >> fileData[i]))
+    {
+      mooseError("Insufficient data in the mesh file");
+    }
   }
   file.close();
 
   // reshape and write into torch tensor
-  for(int64_t i = 0; i <_binary_mesh.size(2); i++)
-    for(int64_t j = 0; j < _binary_mesh.size(1); j++)
-        for(int64_t k = 0; k < _binary_mesh.size(0); k++)
-          _binary_mesh.index_put_({k, j, i}, matrixData[i * _binary_mesh.size(2) + j][k]);
+  for(int64_t k = 0; k < _nz; k++)
+    for(int64_t j = 0; j < _ny; j++)
+        for(int64_t i = 0; i < _nx; i++)
+          _binary_mesh.index_put_({i, j, k}, fileData[k * _ny * _nx + j * _nx + i]);
 }
 
 void
-LatticeBoltzmannMesh::loadMeshFromVTKFile()
+LatticeBoltzmannMesh::loadMeshFromVTKFile(const std::string& filePath,
+    torch::Tensor& binaryMedia,
+    torch::Tensor& poreSize,
+    torch::Tensor& knudsenNumber)
 {
   if (_dim != 2)
     mooseError("VTK mesh reader is only supported for 2D cases");
 
   _console<<COLOR_WHITE<<"Loading Binary Mesh From VTK File\n";
 
-  std::vector<int> dims = {static_cast<int>(_nx),
-                          static_cast<int>(_ny), static_cast<int>(_nz)}; // fixes narrowing conversion warning
+  std::vector<int> dims = {static_cast<int>(_nx), static_cast<int>(_ny), static_cast<int>(_nz)};
 
-  #ifdef SWIFT_HAVE_VTK
-   MooseTensor::read2DStructuredLBMMeshFromVTK(_mesh_file, _binary_mesh, _local_pore_size, _Knudsen_number, dims);
+  #ifdef LIBMESH_HAVE_VTK
+   /*
+      Function to read binary porous media, local pore size distribution and Knudsen number
+      distributions from VTK file. scripts/create_vtk.py MUST be used to generate the vtk file
+    */
+   vtkSmartPointer<vtkXMLStructuredGridReader> reader = vtkSmartPointer<vtkXMLStructuredGridReader>::New();
+   reader->SetFileName(filePath.c_str());
+   reader->Update();
+
+   vtkSmartPointer<vtkStructuredGrid> structuredGrid = reader->GetOutput();
+
+   vtkSmartPointer<vtkDataArray> binaryMediaArray = structuredGrid->GetPointData()->GetArray("BinaryMedia");
+   vtkSmartPointer<vtkDataArray> poreSizeArray = structuredGrid->GetPointData()->GetArray("PoreSize");
+   vtkSmartPointer<vtkDataArray> knudsenNumberArray = structuredGrid->GetPointData()->GetArray("KnudsenNumber");
+
+   for (int64_t i = 0; i < dims[0]; i++)
+     for (int64_t j = 0; j < dims[1]; j++)
+       for (int64_t k = 0; k < dims[2]; k++)
+       {
+         // 2D only, 3D needs to be tested
+         int index = k * dims[1] * dims[2] + i * dims[1] + j;
+         binaryMedia.index_put_({i, j, k}, binaryMediaArray->GetTuple1(index));
+         poreSize.index_put_({i, j, k}, poreSizeArray->GetTuple1(index));
+         knudsenNumber.index_put_({i, j, k}, knudsenNumberArray->GetTuple1(index));
+       }
   #else
     mooseError("VTK not enabled");
   #endif
