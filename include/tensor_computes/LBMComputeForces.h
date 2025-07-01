268f1637da2330b97dfd002a59e89ad1aae2006a
Nijat Nazim Rustamov <nijat.rustamov@inl.gov>
2025-07-01 16:33:13 -0600

    start expanding scope of forces

--- include/tensor_computes/LBMComputeForces.h
+++ include/tensor_computes/LBMComputeForces.h
@@ -18,14 +18,25 @@ class LBMComputeForces : public LatticeBoltzmannOperator
 public:
   static InputParameters validParams();
 
+  void computeBuoyancy();
+  void computeGravity();
+  void computeSurfaceForces();
+  /// more forces can be added ?
+
   LBMComputeForces(const InputParameters & parameters);
 
   void computeBuffer() override;
 
 protected:
-  const torch::Tensor & _temperature;
-  const Real & _density;
-  const Real & _T0;
+  const Real & _reference_density;
+  const Real & _reference_temperature;
+
   const bool _enable_gravity;
+  const bool _enable_buoyancy;
+  const bool _enable_surface_forces;
+
   const Real _g; // gravitational acceleration
+
+  const torch::Tensor & _density_tensor;
+  const torch::Tensor & _temperature;
 };
--- src/tensor_computes/LBMComputeForces.C
+++ src/tensor_computes/LBMComputeForces.C
@@ -18,33 +18,68 @@ LBMComputeForces::validParams()
 {
   InputParameters params = LatticeBoltzmannOperator::validParams();
   // params.addRequiredParam<TensorInputBufferName>("f", "Distribution function");
-  params.addRequiredParam<TensorInputBufferName>("temperature", "Macroscopic temperature");
-  params.addRequiredParam<std::string>("rho0", "Macroscopic density");
-  params.addRequiredParam<std::string>("T0", "Reference temperature");
-  params.addParam<bool>("enable_gravity", false, "Whether to consider gravity or not");
-  params.addParam<std::string>("gravity", "1.0", "Gravitational accelaration");
+  params.addParam<TensorInputBufferName>("temperature", "T", "Macroscopic temperature");
+  params.addParam<TensorInputBufferName>("rho", "rho", "Macroscopic density");
+
+  params.addParam<std::string>("rho0", "1.0", "Reference density");
+  params.addParam<std::string>("T0", "1.0", "Reference temperature");
+  params.addParam<std::string>("gravity", "0.001", "Gravitational accelaration");
+
+  params.addParam<bool>("enable_gravity", false, "Whether to consider gravity");
+  params.addParam<bool>("enable_buoyancy", false, "Whether to consider buoyancy");
+  params.addParam<bool>(
+      "enable_surface_forces", false, "Whether to consider surface tension in multiphase flow");
+
   params.addClassDescription("Compute object for LB forces");
   return params;
 }
 
 LBMComputeForces::LBMComputeForces(const InputParameters & parameters)
-  : LatticeBoltzmannOperator(parameters), /*_f(getInputBuffer("f"))*/
-    _temperature(getInputBufferByName(getParam<TensorInputBufferName>("temperature"))),
-    _density(_lb_problem.getConstant<Real>(getParam<std::string>("rho0"))),
-    _T0(_lb_problem.getConstant<Real>(getParam<std::string>("T0"))),
+  : LatticeBoltzmannOperator(parameters),
+    _reference_density((_lb_problem.getConstant<Real>(getParam<std::string>("rho0")))),
+    _reference_temperature((_lb_problem.getConstant<Real>(getParam<std::string>("T0")))),
     _enable_gravity(getParam<bool>("enable_gravity")),
-    _g(_lb_problem.getConstant<Real>(getParam<std::string>("gravity")))
+    _enable_buoyancy(getParam<bool>("enable_buoyancy")),
+    _enable_surface_forces(getParam<bool>("enable_surface_forces")),
+    _g(_lb_problem.getConstant<Real>(getParam<std::string>("gravity"))),
+    _density_tensor(getInputBufferByName(getParam<TensorInputBufferName>("rho"))),
+    _temperature(getInputBufferByName(getParam<TensorInputBufferName>("temperature")))
 {
+}
+
+void
+LBMComputeForces::computeGravity()
+{
+  _u = _u + _u.index_put_({Slice(), Slice(), Slice(), 1}, _g * _density_tensor);
+}
+
+void
+LBMComputeForces::computeBuoyancy()
+{
+  // Boussinesq approximation of buoyancy
+  _u = _u + _u.index_put_({Slice(), Slice(), Slice(), 1},
+                          (_g * _reference_density) * (_temperature - _reference_temperature));
+}
+
+void
+LBMComputeForces::computeSurfaceForces()
+{
+  // TBD
 }
 
 void
 LBMComputeForces::computeBuffer()
 {
+  _u = torch::zeros_like(_u);
+
   if (_enable_gravity)
-    _u.index_put_({Slice(), Slice(), Slice(), 1}, (_g * _density) * (_temperature - _T0));
-  else
-  {
-    // TBD
-  }
+    computeGravity();
+  if (_enable_buoyancy)
+    computeBuoyancy();
+  if (_enable_surface_forces)
+    computeSurfaceForces();
+
+  /// more forces can be added ?
+
   _lb_problem.maskedFillSolids(_u, 0);
 }
